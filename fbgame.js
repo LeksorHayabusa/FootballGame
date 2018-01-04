0c88242016dac14db8b0b1d684b6573ece389c5e
LeksorHayabusa <alexmotorniyg@gmail.com>
2018-01-04 22:14:18 +0100

    Initial commit

---
+++ fbgame.js
@@ -0,0 +1,33 @@
+
+var myPeer = new Peer({key:'lwjd5qra8257b9'});
+
+$(document).ready(function(){
+$('#getID').on('click', function(){
+	alert('sd');
+
+})
+
+$('#connectToID').on('submit', function(event){
+	event.preventDefault();
+	var otherPIR = $('writeID').val();
+})
+
+	myPeer.on('open', function(id) {
+  		console.log('My peer ID is: ' + id);
+});
+//getElementByID('connectToID').
+var conn = myPeer.connect('dest-peer-id');
+
+conn.on('open', function() {
+  // Receive messages
+  conn.on('data', function(data) {
+    console.log('Received', data);
+  });
+
+  conn.send('hello')
+})
+
+myPeer.on('connection', function(conn) { });
+
+
+}) //ready function
