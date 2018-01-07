52fc0d55afe58690f5e3b05e94a2e0b9d75852a0
Alex Nikonov <31586394+LeksorHayabusa@users.noreply.github.com>
2018-01-07 20:21:36 +0100

    Add files via upload

--- fbgame.js
+++ fbgame.js
@@ -1,67 +1,70 @@
-$(function(){
 
-      var myPeer; // peer object
-      var conn; // connection stored
-
-      // click event listener - click will create new peer (host)!
-      $('#getID').on('click', function(){
-
-        // new host-peer
-        myPeer = new Peer({key:'jg7jctlc81c0izfr', debug: 3});
-        // Here we are create new ID of the connection (our host)
-      	myPeer.on('open', function(id) {
-            console.log('My peer ID is: ' + id);
-        });
-        //  IMPORTANT: Here we are deal with incoming connection
-        //             from remote peer (guest)!!!
-        myPeer.on('connection', function(connection) {
-          // IMPORTANT to handle recieved connection
-          // after this assignment we can use .send() method for our HOST!
-          // conn.send(our message);
-          conn = connection; //can use .send() method for our HOST!
-
-          // Here set listener for incoming messages
-          conn.on('data', function(data){
-            console.log('Recieved - ' + data);
-          });
-        });
-
-    });
-
-    // click event listener - click will connect
-    //  with new peer (guest) to existed peer (host)!
-    $('#connectToID').on('click', function(){
-      // new guest-peer
-      myPeer = new Peer({key:'jg7jctlc81c0izfr', debug: 3});
-      // take host-peer ID from input to create connection
-      let otherPIR = $('#writeID').val();
-      // establish connection with host-peer
-
-      // IMPORTANT to handle recieved connection
-      // after this assignment we can use .send() method for our GUEST!
-      // conn.send(our message);
-      conn = myPeer.connect(otherPIR); //can use .send() method for our GUEST!
-
-
-      // if connection established - send greeting
-      conn.on('open', function(id) {
-            conn.send('Greeting message!');
-            //console.log('My peer ID is: ' + myPeer.id + '. Connected successful!');
-            //console.log('Status: ' + myPeer.open);
-      });
-
-      // Here we listening for incoming messages
-      conn.on('data', function(data){
-        console.log('Recieved - ' + data);
-      });
-
-    });
-
-    // Send message function
-    $('#sendMessage').on('click', function(){
-        // here conn - our HOST or GUEST connection
-        conn.send(prompt('Enter your message: '));
-    });
-
-
+'use strict'
+var myPeer;
+var connectedPeers = {};
+myPeer = new Peer({key:'lwjd5qra8257b9'
+  , debug : 3
+  , logFunction : function(){
+    var copy = Array.prototype.slice.call(arguments).join(' ');
+    $('.log').append(copy + '<br>');
+  }
 });
+
+// a new comment by Dimas
+
+function connect(c){
+  if (c.label === 'chat'){
+    var charbox, header, messages;
+    
+    var chatbox = $('<div></div>').addClass('connection').addClass('active').attr('id', c.peer);
+    var header = $('<h1></h1>').html('Chat with <strong>' + c.peer + '</strong>');
+    var messages = $('<div><em>Peer connected.</em></div>').addClass('messages');
+    chatbox.append(header);
+    chatbox.append(messages);
+  }
+}
+
+$(document).ready(function(){
+    myPeer.on('open', function(id){
+      $('#myID').text('my id is: ' + id);
+    })
+//notation: method connect use homePeerID. and (hostPeerID)
+//homePeerID.connect(hostPeerID, handler???)
+  $('#connectToDestID').on('click', function(event){
+    var destID = $('#destID').val();
+    if(!connectedPeers[destID]){
+      var c = myPeer.connect(destID, {
+        label : 'gameData', 
+        serialization : 'binary-utf8',
+        metaData : {message : 'the game starts now!'}
+      })
+      c.on('open', function(){
+        connect(c);
+      })
+      c.on('error', function(err){
+        alert(err);
+      })
+    }
+  })
+
+
+
+  	myPeer.on('open', function(id) {
+    		console.log('My peer ID is: ' + id);
+  });
+  //getElementByID('connectToID').
+  var conn = myPeer.connect('dest-peer-id');
+
+  conn.on('open', function() {
+    // Receive messages
+    conn.on('data', function(data) {
+      console.log('Received', data);
+    });
+
+    conn.send('hello')
+  })
+
+  myPeer.on('connection', function(conn) { });
+
+
+}) //ready function
---
+++ fbgame_черновик.js
@@ -0,0 +1,95 @@
+'use strict';
+$(function(){
+  var upperLimit = 110;
+  var lowerLimit = 390;
+  $(document).on('keydown', function(e){
+    e.preventDefault();
+      let replacement = $('#goalkeeperLeft').attr('x');
+    if(e.keyCode === 38){
+      console.log(e.keyCode + ' - is pressed');//надо заменитть на доступ к значению по оси Х
+      //по отношению к вратарю
+      if( replacement > upperLimit){
+        replacement++;
+      }
+      //функция передающая перемещиение в чат
+      //функция задающая перемещение вратаря
+    }else if(e.keyCode === 40){
+      console.log(e.keyCode + ' - is pressed')
+      let replacement = 0;
+      if( lowerLimit > replacement){
+        replacement--;
+      }//else do nothing
+      $('#goalkeeperLeft').attr('x',replacement);
+      //функция передающая перемещиение в чат
+      //функция задающая перемещение вратаря
+    }
+    
+  })
+})
+
+/*$(function(){
+
+      var myPeer; // peer object
+      var conn; // connection stored
+
+      // click event listener - click will create new peer (host)!
+      $('#getID').on('click', function(){
+
+        // new host-peer
+        myPeer = new Peer({key:'9l97dfmlbygy14i', debug: 3});
+        // Here we are create new ID of the connection (our host)
+        myPeer.on('open', function(id) {
+            console.log('My peer ID is: ' + id);
+        });
+        //  IMPORTANT: Here we are deal with incoming connection
+        //             from remote peer (guest)!!!
+        myPeer.on('connection', function(connection) {
+          // IMPORTANT to handle recieved connection
+          // after this assignment we can use .send() method for our HOST!
+          // conn.send(our message);
+          conn = connection; //can use .send() method for our HOST!
+
+          // Here set listener for incoming messages
+          conn.on('data', function(data){
+            console.log('Recieved - ' + data);
+          });
+        });//P2P FUNCTION
+
+    });
+
+    // click event listener - click will connect
+    //  with new peer (guest) to existed peer (host)!
+    $('#connectToID').on('click', function(){
+      // new guest-peer
+      myPeer = new Peer({key:'9l97dfmlbygy14i', debug: 3});
+      // take host-peer ID from input to create connection
+      let otherPIR = $('#writeID').val();
+      // establish connection with host-peer
+
+      // IMPORTANT to handle recieved connection
+      // after this assignment we can use .send() method for our GUEST!
+      // conn.send(our message);
+      conn = myPeer.connect(otherPIR); //can use .send() method for our GUEST!
+
+
+      // if connection established - send greeting
+      conn.on('open', function(id) {
+            conn.send('Greeting message!');
+            //console.log('My peer ID is: ' + myPeer.id + '. Connected successful!');
+            //console.log('Status: ' + myPeer.open);
+      });
+
+      // Here we listening for incoming messages
+      conn.on('data', function(data){
+        console.log('Recieved - ' + data);
+      });
+
+    });
+
+    // Send message function
+    $('#sendMessage').on('click', function(){
+        // here conn - our HOST or GUEST connection
+        conn.send(prompt('Enter your message: '));
+    });
+
+}); // ready brackets*/
